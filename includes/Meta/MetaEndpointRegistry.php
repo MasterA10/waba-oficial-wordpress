467cb5f86c776693bfdf184555108ff3dff10eb5
Alex Alves <nasalexalves@gmail.com>
2026-04-27 19:01:41 -0300

    templates-niti

--- includes/Meta/MetaEndpointRegistry.php
+++ includes/Meta/MetaEndpointRegistry.php
@@ -8,10 +8,16 @@ if (!defined('ABSPATH')) {
 class MetaEndpointRegistry {
     public static function resolve(string $operation, array $params = []): string {
         $map = [
-            'messages.send' => '/{phone_number_id}/messages',
-            'media.upload'  => '/{phone_number_id}/media',
-            'media.get'     => '/{media_id}',
-            'media.delete'  => '/{media_id}',
+            'templates.create' => '/{waba_id}/message_templates',
+            'templates.list'   => '/{waba_id}/message_templates',
+            'templates.delete' => '/{waba_id}/message_templates',
+            'templates.update' => '/{template_id}',
+
+            'messages.send'    => '/{phone_number_id}/messages',
+
+            'media.upload'     => '/{phone_number_id}/media',
+            'media.get'        => '/{media_id}',
+            'media.delete'     => '/{media_id}',
         ];
 
         if (!isset($map[$operation])) {
